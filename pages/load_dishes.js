e8c8c991acb84a505a039c3311b9967ff0227f50
skilmee <151780380+skilmee@users.noreply.github.com>
2024-12-17 04:28:33 +0300

    Update load_dishes.js

--- pages/load_dishes.js
+++ pages/load_dishes.js
@@ -2,7 +2,8 @@ const loadDishes = async () => {
     let response = await fetch('https://edu.std-900.ist.mospolytech.ru/labs/api/dishes');
     let menu = await response.json();
     return menu;
-   };
-   
-   export default loadDishes;
-   
+  }
+  
+  export default loadDishes;
+    
+    
