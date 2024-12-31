335ba3148fee98d4bfba4ed3d5739a93bfb7747d
skilmee <151780380+skilmee@users.noreply.github.com>
2024-12-31 21:27:50 +0300

    Update load_dishes.js

--- pages/load_dishes.js
+++ pages/load_dishes.js
@@ -1,4 +1,5 @@
 const loadDishes = async () => {
+<<<<<<< HEAD
   let response = await fetch('https://edu.std-900.ist.mospolytech.ru/labs/api/dishes');
   let menu = await response.json();
   return menu;
@@ -7,3 +8,13 @@ const loadDishes = async () => {
 export default loadDishes;
   
   
+=======
+    let response = await fetch('https://edu.std-900.ist.mospolytech.ru/labs/api/dishes');
+    let menu = await response.json();
+    return menu;
+  }
+  
+  export default loadDishes;
+    
+    
+>>>>>>> parent of 2b5015b (Update load_dishes.js)
